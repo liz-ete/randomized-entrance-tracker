6f90b7773fee1ad67b041f05b3a42c791ea557fa
fili <filistea@protonmail.com>
2024-07-16 11:19:31 -0700

    Add images

---
+++ src/assets/images.js
@@ -0,0 +1,48 @@
+export const itemsImages = {
+  bottle: require('./images/bottle.webp'),
+  blue_cane: require('./images/blue_cane.webp'),
+  bombos: require('./images/bombos.webp'),
+  bow: require('./images/bow.webp'),
+  cape: require('./images/cape.webp'),
+  compass: require('./images/compass.webp'),
+  crystal: require('./images/crystal.webp'),
+  ether: require('./images/ether.webp'),
+  fire_rod: require('./images/fire_rod.webp'),
+  flippers: require('./images/flippers.webp'),
+  green_pendant: require('./images/green_pendant.webp'),
+  hammer: require('./images/hammer.webp'),
+  hookshot: require('./images/hookshot.webp'),
+  ice_rod: require('./images/ice_rod.webp'),
+  lamp: require('./images/lamp.webp'),
+  map: require('./images/map.webp'),
+  mirror: require('./images/mirror.webp'),
+  mitts: require('./images/mitts.webp'),
+  moon_pearl: require('./images/moon_pearl.webp'),
+  net: require('./images/net.webp'),
+  powder: require('./images/powder.webp'),
+  quake: require('./images/quake.webp'),
+  red_bomb: require('./images/red_bomb.webp'),
+  red_pendant: require('./images/red_pendant.webp'),
+  shovel: require('./images/shovel.webp'),
+  shroom: require('./images/shroom.webp'),
+  small_key: require('./images/small_key.webp'),
+  somaria: require('./images/somaria.webp'),
+  sword: require('./images/sword.webp'),
+  titans_mitts: require('./images/titans_mitts.webp'),
+  bomb: require('./images/bomb.webp'),
+  big_key: require('./images/big_key.webp'),
+};
+
+export const locationsImages = {
+  ep: require('./images/ep.png'),
+  sp: require('./images/ep.png'),
+};
+
+export const others = {
+  warp: require('./images/warp.png'),
+};
+
+export const mapsImages = {
+  lightWorld: require('./images/Lightworldalttp.png'),
+  darkWorld: require('./images/Darkworldalttp.webp'),
+};
