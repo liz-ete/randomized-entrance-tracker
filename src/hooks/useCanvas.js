b32f9a2ed20e693d5ac4ef68634ae64dd39a8d78
liz-ete <garcia.amano.dl@gmail.com>
2024-07-24 23:44:41 -0700

    Add useCallback hook for better performance

--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -1,28 +1,25 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 function useCanvas(draw) {
   const canvasRef = useRef(null);
+
+  const updateCanvasSize = useCallback((canvas, context) => {
+    const { width, height } = canvas.getBoundingClientRect();
+    const { devicePixelRatio: ratio = 1 } = window;
+
+    if (canvas.width !== width || canvas.height !== height) {
+      canvas.width = width * ratio;
+      canvas.height = height * ratio;
+      context.scale(ratio, ratio);
+    }
+  }, []);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     updateCanvasSize(canvas, ctx);
     draw(ctx);
-  }, [draw]);
-
-  const updateCanvasSize = (canvas, context) => {
-    const { width, height } = canvas.getBoundingClientRect();
-
-    if (canvas.width !== width || canvas.height !== height) {
-      const { devicePixelRatio: ratio = 1 } = window;
-      canvas.width = width * ratio;
-      canvas.height = height * ratio;
-      context.scale(ratio, ratio);
-      return true;
-    }
-
-    return false;
-  };
+  }, [draw, updateCanvasSize]);
 
   return canvasRef;
 }
