46b2ef13f66974956bd113667e991389ff369445
liz-ete <garcia.amano.dl@gmail.com>
2024-08-13 23:51:02 -0700

    Remove canvas resizing functionality

--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -1,25 +1,13 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 function useCanvas(draw) {
   const canvasRef = useRef(null);
 
-  const updateCanvasSize = useCallback((canvas, context) => {
-    const { width, height } = canvas.getBoundingClientRect();
-    const { devicePixelRatio: ratio = 1 } = window;
-
-    if (canvas.width !== width || canvas.height !== height) {
-      canvas.width = width * ratio;
-      canvas.height = height * ratio;
-      context.scale(ratio, ratio);
-    }
-  }, []);
-
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    updateCanvasSize(canvas, ctx);
     draw(ctx);
-  }, [draw, updateCanvasSize]);
+  }, [draw]);
 
   return canvasRef;
 }
